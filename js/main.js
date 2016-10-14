b1ff321f04f78cfef7169d24dfea7a811efc28a2
angelfeliz <erodriguezfeliz@gmail.com>
2016-10-13 20:28:05 -0400

    alphabet in the index view and on the js the function that set the secret work in the index view

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-  
+var secret_word = "hello";
+
+
+var splitWordAddToView =  function(word){  
+     for(var letter in secret_word){
+        console.log(letter + ' ' + secret_word[letter]);
+        $('#main_play_area').append('<div class="letter_box letter_' + letter +' "></div>' );
+     }
+
+  }
+splitWordAddToView();
 
 });
